0d1b0aceea768ff29e7bed4a8415783fe59c43c1
Andreas Gutheil <andreas.gutheil@namics.com>
2015-05-12 00:36:47 +0200

    refactoring

--- checkout/src/main/java/hello/MyConfig.java
+++ checkout/src/main/java/hello/MyConfig.java
@@ -1,15 +1,7 @@
 package hello;
 
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.social.oauth2.OAuth2Template;
-import sun.net.www.http.HttpClient;
 
 /**
  * Created by agutheil on 11.05.15.
@@ -19,17 +11,7 @@ public class MyConfig {
     @Bean
     public MightyCore restTemplate(){
         MightyCore mightyCore = new MightyCore("mightymerceapp","mySecretOAuthSecret","http://localhost:8080/oauth/authorize", "http://localhost:8080/oauth/authenticate", "http://localhost:8080/oauth/token");
-        mightyCore.setUseParametersForClientAuthentication(true);
-        mightyCore.setRequestFactory(createSecureTransport("admin","admin"));
+        mightyCore.setUseParametersForClientAuthentication(false);
         return mightyCore;
-    }
-
-    public ClientHttpRequestFactory createSecureTransport(final String username, final String password) {
-        DefaultHttpClient httpClient = new DefaultHttpClient();
-        BasicCredentialsProvider credentialsProvider =  new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
-        httpClient.setCredentialsProvider(credentialsProvider);
-        ClientHttpRequestFactory rf = new HttpComponentsClientHttpRequestFactory(httpClient);
-        return rf;
     }
 }
