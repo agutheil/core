71f1cf625927381844d409144d32490b44990656
Andreas Gutheil <agutheil@c12320.local>
2015-07-31 12:51:29 +0200

    facebook js und api integration

--- src/main/webapp/scripts/app/app.js
+++ src/main/webapp/scripts/app/app.js
@@ -44,6 +44,73 @@ angular.module('mightymerceApp', ['LocalStorageModule', 'tmh.dynamicLocale',
                 $state.go($rootScope.previousStateName, $rootScope.previousStateParams);
             }
         };
+
+        $window.fbAsyncInit = function() {
+            // Executed when the SDK is loaded
+
+            FB.init({ 
+
+              /* 
+               The app id of the web app;
+               To register a new app visit Facebook App Dashboard
+               ( https://developers.facebook.com/apps/ ) 
+              */
+
+              appId: '404823203038294', 
+
+              /* 
+               Adding a Channel File improves the performance 
+               of the javascript SDK, by addressing issues 
+               with cross-domain communication in certain browsers. 
+              */
+
+              //channelUrl: 'app/channel.html', 
+
+              /* 
+               Set if you want to check the authentication status
+               at the start up of the app 
+              */
+
+              status: true, 
+
+              /* 
+               Enable cookies to allow the server to access 
+               the session 
+              */
+
+              cookie: true, 
+
+              /* Parse XFBML */
+
+              xfbml: true,
+              /*
+               use version 2.3 
+              */
+              version: 'v2.3' 
+            });
+            //sAuth.watchAuthenticationStatusChange();
+        };
+        // Are you familiar to IIFE ( http://bit.ly/iifewdb ) ?
+
+          (function(d){
+            // load the Facebook javascript SDK
+
+            var js, 
+            id = 'facebook-jssdk', 
+            ref = d.getElementsByTagName('script')[0];
+
+            if (d.getElementById(id)) {
+              return;
+            }
+
+            js = d.createElement('script'); 
+            js.id = id; 
+            js.async = true;
+            js.src = "//connect.facebook.net/en_US/all.js";
+
+            ref.parentNode.insertBefore(js, ref);
+
+          }(document));
     })
     
     .factory('authInterceptor', function ($rootScope, $q, $location, localStorageService) {
--- src/main/webapp/scripts/app/entities/merchantChannel/merchantChannel-detail.controller.js
+++ src/main/webapp/scripts/app/entities/merchantChannel/merchantChannel-detail.controller.js
@@ -1,12 +1,20 @@
 'use strict';
 
 angular.module('mightymerceApp')
-    .controller('MerchantChannelDetailController', function ($scope, $stateParams, MerchantChannel, Merchant, Channel) {
+    .controller('MerchantChannelDetailController', function ($scope, $stateParams, MerchantChannel, Merchant, Channel, Facebook) {
         $scope.merchantChannel = {};
         $scope.load = function (id) {
             MerchantChannel.get({id: id}, function(result) {
               $scope.merchantChannel = result;
             });
         };
+        $scope.getMyLastName = function() {
+            Facebook.getMyLastName()
+                .then(function(response) {
+                    $scope.last_name = response.last_name;
+                    console.log($scope.last_name)
+            });
+        };
         $scope.load($stateParams.id);
+        $scope.getMyLastName();
     });
